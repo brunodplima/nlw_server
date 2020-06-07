657cfe4120aced71aeabd0d2417f8a035f26530b
Bruno Lima <brunodplima@gmail.com>
2020-06-06 22:54:04 -0300

    Configurar validação

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,6 @@
 import express from 'express'
+import { celebrate, Joi } from 'celebrate'
+
 import multer from 'multer'
 import multerConfig from './config/multer'
 
@@ -13,7 +15,24 @@ const pointController = new PointController()
 
 routes.get('/items', itemController.index)
 
-routes.post('/points', upload.single('image'), pointController.create)
+routes.post('/points',
+  upload.single('image'),
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().required().email(),
+      whatsapp: Joi.number().required(),
+      latitude: Joi.number().required(),
+      longitude: Joi.number().required(),
+      city: Joi.number().required(),
+      uf: Joi.number().required().min(2).max(2),
+      items: Joi.string().required(), //TODO: regex
+    })
+  }, {
+    abortEarly: false,
+  }),
+  pointController.create
+)
 routes.get('/points/:id', pointController.show)
 routes.get('/points', pointController.index)
 
